6271e1e7f1b6ba067c82cf3d1698e38ac3670752
damoore <damoore@microsoft.com>
2015-05-04 08:50:18 -0700

    do intersection

--- r-script.R
+++ r-script.R
@@ -16,5 +16,27 @@ lastfmdata<-read.csv("lastfm-dataset-as-csv.csv", header = FALSE)
 names(lastfmdata)<-c("User", "ArtistId", "Artist","Plays")
 
 
+
 #takes a long time
 splitlastfm<-split(lastfmdata$User, lastfmdata$Artist)
+
+matchup <- data.frame(artist1=character(),
+                      artist2=character(),
+                      intersection=numeric())
+
+commonusersthreshold <- 100
+
+# This takes way to long
+for(i in 1:length(splitlastfm))
+{
+    for(j in 1:length(splitlastfm))
+    {
+        if(length(intersect(splitlastfm[[i]], splitlastfm[[j]])) > commonusersthreshold) 
+        {
+            matchup<-rbind(matchup, 
+                    c(names(splitlastfm[i]), 
+                    names(splitlastfm[j]), 
+                    length(intersect(splitlastfm[[i]], splitlastfm[[j]]))))
+        }
+    }
+}
