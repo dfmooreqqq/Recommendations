ab972ded7b4b4d0b9be4fa23e1ddd9b831ce3c9a
Daniel Moore <dfmoore@gmail.com>
2015-05-03 21:11:51 -0700

    subset as csv and loading the data script

---
+++ r-script.R
@@ -0,0 +1,16 @@
+## Environment setup
+# Load packages.
+packages <- c("gdata", "ggplot2")
+packages <- lapply(packages, FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+        install.packages(x)
+        library(x, character.only = TRUE)
+    }
+})
+
+# Set working directory
+workingdir<-paste("C:\\Users", Sys.getenv("USERNAME"), "Documents\\GitHub\\Recommendations", sep = "\\")
+setwd(workingdir)
+
+lastfmdata<-read.csv("lastfm-dataset-as-csv.csv", header = FALSE)
+names(lastfmdata)<-c("User", "ArtistId", "Artist","Plays")
